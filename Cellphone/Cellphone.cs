7ec9dfe0f14ddd987e4718468800f9d8dc6cb9fe
Greg Doud <gpdoud@gmail.com>
2021-02-15 15:38:19 -0500

    final commit

---
+++ Cellphone/Cellphone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cellphone {
+
+    class Cellphone {
+
+        public string PhoneNumber { get; set; }
+        public string ServiceProvider { get; set; }
+        public string OperatingSystem { get; set; }
+        public bool Active { get; set; } = true;
+        public int Capacity { get; set; }
+        public string Model { get; set; }
+
+    }
+}
--- Cellphone/Program.cs
+++ Cellphone/Program.cs
@@ -3,7 +3,35 @@
 namespace Cellphone {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var cp1 = new Cellphone {
+                PhoneNumber = "513-555-1212",
+                ServiceProvider = "Verizon",
+                OperatingSystem = "iOS",
+                Capacity = 128,
+                Model = "iPhone X"
+            };
+            var cp2 = new Cellphone {
+                PhoneNumber = "614-555-1212",
+                ServiceProvider = "T-Mobile",
+                OperatingSystem = "Android",
+                Capacity = 64,
+                Model = "Samsung"
+            };
+            var cp3 = new Cellphone {
+                PhoneNumber = "216-555-1212",
+                ServiceProvider = "T-Mobile",
+                OperatingSystem = "Android",
+                Capacity = 256,
+                Model = "Samsung"
+            };
+            
+            var cellphones = new Cellphone[] { cp1, cp2, cp3 };
+
+            foreach(var cp in cellphones) {
+                Console.WriteLine($"phone number is {cp.PhoneNumber}");
+            }
         }
     }
+
 }
